289992e9bcad7f7306dbb1f036108b46680d6cf7
zombielifestyle <zombielifestyle@gmail.com>
2012-08-13 23:24:28 +0200

    Initial commit.

---
+++ overscore.php
@@ -0,0 +1,400 @@
+<?php
+
+function _bind($f, $context){
+    if ($f instanceof Closure) {
+        return $f->bindTo($context);
+    }
+    return $f;
+}
+
+function _call($f, $context) {
+    if (is_object($context)) {
+        $f = _bind($f, $context);
+    }
+    $args = array_slice(func_get_args(), 2);
+    return call_user_func_array($f, $args);
+}
+
+function _apply($f, $context, $args = array()){
+    if (is_object($context)) {
+        $f = _bind($f, $context);
+    }  
+    return call_user_func_array($f, $args);
+}
+
+function _memoize($f) {
+    return function() use($f) {
+        static $memo = array();
+        $args = func_get_args();
+        $key = md5(serialize($args));
+        if (!isset($memo[$key])) {
+            $memo[$key] = _apply($f, null, $args);
+        }
+        return $memo[$key];
+    };
+}
+
+function _once($f) {
+    return function() use($f) {
+        static $called = false, $returnValue = null;
+        if (!$called) {
+            $called = true;
+            $returnValue = $f();
+        }
+        return $returnValue;
+    };
+}
+
+function _after($count, $f) {
+    return function() use($count, $f) {
+        static $calls = 0, $returnValue;
+        if (++$calls == $count) {
+            $returnValue = $f();
+        }
+        return $returnValue;
+    };
+}
+
+function _wrap($f, $wrapper){
+    return function() use($f, $wrapper) {
+        return $wrapper($f);
+    };
+}
+
+function _compose() {
+    $functions = func_get_args();
+    return function($value) use($functions) {
+        foreach ($functions as $function) {
+            $value = $function($value);
+        }
+        return $value;
+    };
+}
+
+function _keys($o) {
+    if (is_object($o)) {
+        $o = get_object_vars($o);
+    }
+    return array_keys($o);
+}
+
+function _values($o) {
+    if (is_object($o)) {
+        $o = get_object_vars($o);
+    }
+    return array_values($o);
+}
+
+function _functions($o) {
+    $class = null;
+    if (is_object($o)) {
+        $class = get_class($o);
+    } else if (is_string($o) && class_exists($o)) {
+        $class = $o;
+    }
+    if ($class) {
+        return get_class_methods($class);
+    }
+}
+
+function _extends(){
+    $args = func_get_args();
+    return _apply('array_merge', null, $args);
+}
+
+function _pick($haystack) {
+    $needles = array_flip(array_slice(func_get_args(), 1));
+    return array_intersect_key($haystack, $needles);
+}
+
+function _defaults($a, $defaults) {
+    foreach ($defaults as $key => $value) {
+        if (!array_key_exists($key, $a)) {
+            $a[$key] = $value;
+        }
+    }
+    return $a;
+}
+
+function _has($a, $key){
+    if (is_array($a)) {
+        return array_key_exists($key, $a);
+    } 
+    else if (is_object($a)) {
+        return in_array($key, _keys($a)); 
+    }
+    return null;
+}
+
+function _empty($a) {
+    return empty($a);
+}
+
+function _identity($identity) {
+    return $identity;
+}
+
+function _times($count, $f) {
+    while ($count-- > 0) {
+        $f();
+    }
+}
+
+function _uniqueId($prefix = null) {
+    static $ids = array();
+    if (!_has($ids, $prefix)) {
+        $ids[$prefix] = 0;
+    }
+    return $prefix.++$ids[$prefix];
+}
+
+function _escape($str){
+    return htmlentities($str);
+}
+
+function _result($a, $key) {
+    $value = null;
+    if (array_key_exists($key, $a)) {
+        if (is_array($a)) {
+            $value = $a[$key];
+            if ($value instanceof Closure) {
+                $value = $value();
+            }
+        }
+    }
+    return $value;
+}
+
+function _each($list, $iterator, $context = null) {
+    foreach ($list as $key => $value) {
+        _call($iterator, $context, $value, $key);
+    }
+}
+
+function _map($list, $iterator, $context = null) {
+    foreach ($list as &$value) {
+        $value = _call($iterator, $context, $value);
+    }
+    return $list;
+}
+
+function _reduce($list, $iterator, $memo, $context = null) {
+    while ($item = array_shift($list)) {
+        $memo = _call($iterator, $context, $memo, $item);
+    }
+    return $memo;
+}
+
+function _reduceRight($list, $iterator, $memo, $context = null) {
+    while ($item = array_pop($list)) {
+        $memo = _call($iterator, $context, $memo, $item);
+    }
+    return $memo;
+}
+
+function _find($list, $iterator, $context = null) {
+    foreach ($list as $value) {
+        if (_call($iterator, $context, $value)) {
+            return $value;
+        }
+    }
+}
+
+function _filter($list, $iterator, $context = null) {
+    $results = array();
+    foreach ($list as $value) {
+        if (_call($iterator, $context, $value)) {
+            $results[] = $value;
+        }
+    }
+    return $results;
+}
+
+function _reject($list, $iterator, $context = null) {
+    $results = array();
+    foreach ($list as $value) {
+        if (!_call($iterator, $context, $value)) {
+            $results[] = $value;
+        }
+    }
+    return $results;
+}
+
+function _all($list, $iterator = '_identity', $context = null) {
+    foreach ($list as $value) {
+        if (!_call($iterator, $context, $value)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function _any($list, $iterator = '_identity', $context = null) {
+    foreach ($list as $value) {
+        if (_call($iterator, $context, $value)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function _include($list, $value){
+    return in_array($value, $list);
+}
+
+function _invoke($list, $method) {
+    foreach ($list as $item) {
+        _call(array($item, $method), null);
+    }
+}
+
+function _pluck($list, $key){
+    $result = array();
+    foreach ($list as $item) {
+        if (_has($item, $key)) {
+            $result[] = $item[$key];
+        }
+    }
+    return $result;
+}
+
+function _max($list, $iterator = '_identity', $context = null){
+    $max = 0;
+    foreach ($list as $item) {
+        $max = max(_call($iterator, $context, $item), $max);
+    }
+    return $max;
+}
+
+function _min($list, $iterator = '_identity', $context = null){
+    $min = PHP_INT_MAX;
+    foreach ($list as $item) {
+        $min = min(_call($iterator, $context, $item), $min);
+    }
+    return $min;
+}
+
+function sortBy($list, $iterator, $context) {
+}
+
+function groupBy($list, $iterator, $context) {
+}
+
+function sortedIndex($list, $value, $iterator) {
+}
+
+// function _append($list) {
+//     $args = _rest(func_get_args());
+//     foreach ($args as $arg) {
+//         $list = array_merge($list, (array)$arg);
+//     }
+//     return $list;
+// }
+// 
+// function _prepend($list) {
+//     $args = array_reverse(_rest(func_get_args()));
+//     foreach ($args as $arg) {
+//         $list = array_merge((array)$arg, $list);
+//     }
+//     return $list;
+// }
+
+function _first($list, $n = 1) {
+    if ($n == 1) {
+        return reset($list);
+    }
+    return array_slice($list, 0, $n);
+}
+
+function _initial($list, $n = 1) {
+    return array_slice($list, 0, count($list)-$n);
+}
+
+function _last($list, $n = 1) {
+    if ($n == 1) {
+        return end($list);
+    }
+    return array_slice($list, -$n);
+}
+
+function _rest($list, $n = 1) {
+    return array_slice($list, $n);
+}
+
+function _compact($list) {
+    $result = array();
+    foreach ($list as $value) {
+        if (!empty($value)) {
+            $result[] = $value;
+        }
+    }
+    return $result;
+}
+
+function _flatten($list) {
+    $result = array();
+    array_walk_recursive($list, function($value) use (&$result) { 
+        $result[] = $value; 
+    });
+    return $result;
+}    
+
+function _without($list) {
+    $args = _rest(func_get_args());
+    $result = array();
+    foreach ($list as $value) {
+        if (!in_array($value, $args, true)) {
+            $result[] = $value;
+        }
+    }
+    return $result;
+}
+
+function _with($list) {
+    $args = _rest(func_get_args());
+    foreach ($args as $value) {
+        if (!in_array($value, $list, true)) {
+            $list[] = $value;
+        }
+    }
+    return $list;
+
+}
+
+function _union() {
+    $args = func_get_args();
+    $result = array();
+    foreach ($args as $arg) {
+        $result = array_unique(array_merge($result, $arg)); 
+    }
+    return $result;
+}
+
+function _intersection() {
+    return array_values(_apply('array_intersect', null, func_get_args()));
+}
+
+function _uniq($list) {
+    return array_unique($list);
+}
+
+function _zip() {
+    return _apply('array_map', null, array_merge(array(null), func_get_args()));
+}
+
+function _indexOf($list, $value) {
+    $result = array_search($value, $list);
+    return $result === false ? -1 : $result;
+}
+
+function _lastIndexOf($list, $value) {
+    $index = -1;
+    foreach ($list as $i => $item) {
+        if ($item === $value) {
+            $index = $i;
+        }
+    }
+    return $index;
+}
+
---
+++ overscore_test.php
@@ -0,0 +1,529 @@
+<?php
+
+require 'overscore.php';
+
+class InvokeMe {
+    public $invoked = false;
+    function invoke($arg = true){
+        $this->invoked = $arg;
+    }
+}
+
+class overscore_test extends PHPUnit_Framework_TestCase  {
+
+    function testBind() {
+        $f = function(){
+            return $this->fro;
+        };
+        $o = (object) array('fro' => 'zzle');
+        $f2 = _bind($f, $o);
+        $this->assertEquals('zzle', $f2());
+    }
+
+    function testCall() {
+         $value = _call(function($hello, $world){
+            return "$hello $world";
+        }, null, 'hello', 'world');
+        $this->assertEquals("hello world", $value);
+    }
+
+    function testCallAndBind() {
+        $o = (object) array('world' => 'world');
+        $value = _call(function($hello, $world){
+            return "$hello $this->world";
+        }, $o, 'hello', 'world');
+        $this->assertEquals("hello world", $value);
+    }
+
+    function testApply() {
+         $value = _apply(function($world){
+            return "hello $world";
+        }, null, array('world'));
+        $this->assertEquals("hello world", $value);
+    }
+
+    function testApplyAndBind() {
+        $o = (object) array('world' => 'world');
+        $value = _apply(function(){
+            return "hello $this->world";
+        }, $o, array('world'));
+        $this->assertEquals("hello world", $value);
+    }
+
+    function testMemoize(){
+        $calls = 0;
+        $f = function () use(&$calls) {
+            $calls++;
+            $args = func_get_args();
+            return implode(',', $args);
+        };
+        $memo = _memoize($f);
+        $this->assertEquals("1,2", $memo(1,2));
+        $this->assertEquals("1,2", $memo(1,2));
+        $this->assertEquals("1,2,3", $memo(1,2,3));
+        $this->assertEquals(2, $calls);
+    }
+
+    function testOnce(){
+        $f = function(){
+            static $calls = 0;
+            return ++$calls;
+        };
+        $f2 = _once($f);
+        $f2();
+        $this->assertEquals(1, $f2());
+    }
+
+    function testAfter() {
+        $f = function(){
+            return 'finally called!';
+        };
+        $f2 = _after(2, $f);
+        $this->assertNull($f2());
+        $this->assertEquals('finally called!', $f2());
+    }
+
+    function testWrap() {
+        $f = function() {
+            return 'world';
+        };
+        $f2 = _wrap($f, function($f) {
+            return 'hello '.$f().'!';
+        });
+        $this->assertEquals('hello world!', $f2());
+    }
+
+    function testCompose() {
+        $greet = function($who) {
+            return "hello $who";
+        };
+        $smart = function($greet) {
+            return "$greet, you smartass!";
+        };
+        $f = _compose($greet, $smart);
+        $this->assertEquals('hello harry, you smartass!', $f('harry'));
+    }
+
+    function testKeys() {
+        $o = array('first' => 'me', 'second' => 'you');
+        $this->assertEquals(array('first','second'), _keys($o));
+        $o = (object)$o;
+        $this->assertEquals(array('first','second'), _keys($o));
+    }
+
+    function testValues() {
+        $o = array('first' => 'me', 'second' => 'you');
+        $this->assertEquals(array('me','you'), _values($o));
+        $o = (object)$o;
+        $this->assertEquals(array('me','you'), _values($o));
+    }
+
+    function testFunctions() {
+        $this->assertEquals(array('close','rewind', 'read'), _functions('Directory'));
+        $this->assertEquals(array('close','rewind', 'read'), _functions(dir(__DIR__)));
+    }
+
+    function testExtends() {
+        $a1 = array('a' => 1, 'b' => 'b');
+        $a2 = array('a' => 2);
+        $a3 = array('a' => 3, 'c' => 'c');
+        $a = _extends($a1, $a2, $a3);
+        $expected = array('a' => 3, 'b' => 'b', 'c' => 'c');
+        $this->assertEquals($expected, $a);
+    }
+
+    function testPick() {
+        $a = array('a' => 'a', 'b' => 'b', 'c' => 'c');
+        $expected = array('a' => 'a', 'c' => 'c');
+        $this->assertEquals($expected, _pick($a, 'a', 'c'));
+    }
+
+    function testDefaults(){
+        $a = array('b' => 'b');
+        $defaults = array('a' => 'a', 'c' => 'c');
+        $expected = array('a' => 'a', 'b' => 'b', 'c' => 'c');
+        $result = _defaults($a, $defaults);
+        $this->assertEquals($expected, $result);
+        $result['a'] = 'd';
+        $expected['a'] = 'd';
+        $this->assertEquals($expected, _defaults($result, $defaults));
+    }
+
+    function testHas() {
+        $a = array('a' => 'a');
+        $this->assertTrue(_has($a, 'a'));
+        $this->assertFalse(_has($a, 'b'));
+        $a = (object)$a;
+        $this->assertTrue(_has($a, 'a'));
+        $this->assertFalse(_has($a, 'b'));
+    }
+
+    function testEmpty() {
+        $this->assertTrue(_empty(array()));
+        $this->assertTrue(_empty(0));
+        $this->assertFalse(_empty(array(1)));
+    }
+
+    function testIdentity() {
+        $this->assertEquals('a', _identity('a'));
+        $this->assertNotEquals('a', _identity('b'));
+    }
+
+    function testTimes() {
+        $calls = 0;
+        $f = function () use(&$calls) {
+            ++$calls;
+        };
+        _times(3, $f);
+        $this->assertEquals(3, $calls);
+    }
+
+    function testUniqueId() {
+        $this->assertEquals('foo_1', _uniqueId('foo_'));
+        $this->assertEquals('foo_2', _uniqueId('foo_'));
+        $this->assertEquals(1, _uniqueId());
+        $this->assertEquals(2, _uniqueId());
+    }
+
+    function testEscape(){
+        $this->assertEquals('&lt;foo', _escape('<foo'));
+    }
+
+    function testResult(){
+        $a = array('a' => 'a', 'b' => function(){return 'b';});
+        $this->assertEquals('a', _result($a, 'a'));
+        $this->assertEquals('b', _result($a, 'b'));
+    }
+
+    function testEachWithData(){
+        $result = array();
+        $f = function($value, $index) use(&$result) {
+            $result[] = "$value-$index";
+        };
+        _each(array(1,2), $f);
+        $this->assertEquals(array("1-0", "2-1"), $result);
+    }
+
+    function testEachWithNoData(){
+        $result = array();
+        $f = function($value, $index) use(&$result) {
+            $result[] = "$value-$index";
+        };
+        _each(array(), $f);
+        $this->assertEquals(array(), $result);
+    }
+
+    function testEachWithContext(){
+        $o = (object) array('result' => array());
+        _each(array(1,2), function($value, $index) {
+            $this->result[] = "$value-$index";
+        }, $o);
+        $this->assertEquals(array("1-0", "2-1"), $o->result);
+    }
+
+    function testMap(){
+        $result = _map(array('a', 'b'), function($a){
+            return strtoupper($a);
+        });
+        $this->assertEquals(array('A','B'), $result);
+    }
+
+    function testMapWithNoData(){
+        $result = _map(array(), function($a){
+            return strtoupper($a);
+        });
+        $this->assertEquals(array(), $result);
+    }
+
+    function testMapWithContext(){
+        $o = (object) array('prefix' => '-');
+        $result = _map(array('a', 'b'), function($a){
+            return $this->prefix.$a;
+        }, $o);
+        $this->assertEquals(array('-a','-b'), $result);
+    }
+
+    function testReduce() {
+        $result = _reduce(array(1,2,3), function($memo, $value){
+            return "$memo$value";
+        }, 0);
+        $this->assertEquals('0123', $result);
+    }
+
+    function testReduceWithNoData() {
+        $result = _reduce(array(), function($memo, $value){
+            return "$memo$value";
+        }, 0);
+        $this->assertEquals(0, $result);
+    }
+
+    function testReduceWithContext() {
+        $o = (object) array('sep' => '-');
+        $result = _reduce(array(1,2,3), function($memo, $value){
+            return "$memo$this->sep$value";
+        }, 0, $o);
+        $this->assertEquals('0-1-2-3', $result);
+    }
+
+    function testReduceRight() {
+        $result = _reduceRight(array(1,2,3), function($memo, $value){
+            return "$memo$value";
+        }, 0);
+        $this->assertEquals('0321', $result);
+    }
+
+    function testReduceRightWithNoData() {
+        $result = _reduceRight(array(), function($memo, $value){
+            return "$memo$value";
+        }, 0);
+        $this->assertEquals(0, $result);
+    }
+
+    function testReduceRightWithContext() {
+        $o = (object) array('sep' => '-');
+        $result = _reduceRight(array(1,2,3), function($memo, $value){
+            return "$memo$this->sep$value";
+        }, 0, $o);
+        $this->assertEquals('0-3-2-1', $result);
+    }
+
+    function testFind() {
+        $result = _find(array(1,2,3), function($value){
+            return $value == 2;
+        });
+        $this->assertEquals(2, $result);
+    }
+
+    function testFindWithNoData() {
+        $result = _find(array(), function($value){
+            return $value == 2;
+        });
+        $this->assertEquals(null, $result);
+    }
+
+    function testFindWithContext() {
+        $o = (object) array('cmp' => 2);
+        $result = _find(array(1,2,3), function($value){
+            return $value == $this->cmp;
+        }, $o);
+        $this->assertEquals(2, $result);
+    }
+
+    function testFilter() {
+        $result = _filter(array(1,'a',2), function($value) {
+            return is_int($value);
+        });
+        $this->assertEquals(array(1,2), $result);
+    }
+
+    function testFilterWithNoData() {
+        $result = _filter(array(), function($value) {
+            return is_int($value);
+        });
+        $this->assertEquals(array(), $result);
+    }
+
+    function testFilterWithContext() {
+        $o = (object) array('cmp' => 1);
+        $result = _filter(array(1,'a',2), function($value) {
+            return is_int($value) && $value > $this->cmp;
+        }, $o);
+        $this->assertEquals(array(2), $result);
+    }
+
+    function testReject() {
+        $result = _reject(array(1,'a',2), function($value) {
+            return is_int($value);
+        });
+        $this->assertEquals(array('a'), $result);
+    }
+
+    function testRejectWithNoData() {
+        $result = _reject(array(), function($value) {
+            return is_int($value);
+        });
+        $this->assertEquals(array(), $result);
+    }
+
+    function testRejectWithContext() {
+        $o = (object) array('cmp' => 1);
+        $result = _reject(array(1,'a',2), function($value) {
+            return is_int($value) && $value > $this->cmp;
+        }, $o);
+        $this->assertEquals(array(1,'a'), $result);
+    }
+
+    function testAll(){
+        $this->assertFalse(_all(array(1,0)));
+        $this->assertFalse(_all(array(true,'')));
+        $this->assertTrue(_all(array(true,1,'a',array(1))));
+        $this->assertTrue(_all(array()));
+        $this->assertTrue(_all(array(5,6), function($v){return $v > 4;}));
+    }
+
+    function testAny(){
+        $this->assertFalse(_any(array()));
+        $this->assertFalse(_any(array('',0,false,null)));
+        $this->assertTrue(_any(array(0,true)));
+        $this->assertTrue(_any(array(5,6), function($v){return $v > 4;}));
+    }
+
+    function testInclude(){
+        $this->assertTrue(_include(array('baz','foo'), 'foo'));
+        $this->assertTrue(_include(array('baz', 'voo' => 'foo'), 'foo'));
+        $this->assertFalse(_include(array('baz','foop'), 'foo'));
+    }
+
+    function testInvoke(){
+        $list = array(new InvokeMe(), new InvokeMe());
+        _invoke($list, 'invoke');
+        $this->assertEquals(true, $list[0]->invoked);
+        $this->assertEquals(true, $list[1]->invoked);
+    }
+
+    function testPluck(){
+        $a = array(array('a' => 'a1'), array('a' => 'a2'));
+        $this->assertEquals(array('a1','a2'), _pluck($a, 'a'));
+    }
+
+    function testMax() {
+        $list = array(64, 12, 19, 101);
+        $this->assertEquals(101, _max($list));
+        $this->assertEquals(303, _max($list, function($value){return $value*3;}));
+    }
+
+    function testMin() {
+        $list = array(64, 12, 19, 101);
+        $this->assertEquals(12, _min($list));
+        $this->assertEquals(36, _min($list, function($value){return $value*3;}));
+    }
+
+    function testSortBy() {
+        $this->markTestIncomplete();
+    }
+
+    function testCountBy() {
+        $this->markTestIncomplete();
+    }
+
+    function testGroupBy() {
+        $this->markTestIncomplete();
+    }
+
+    function tesSortedIndex() {
+        $this->markTestIncomplete();
+    }
+
+    // function testAppend() {
+    //     $list = array(1,2,3);
+    //     $this->assertEquals(array(1,2,3), _append($list));
+    //     $this->assertEquals(array(1,2,3,12), _append($list, 12));
+    //     $this->assertEquals(array(1,2,3,12,16), _append($list, 12, 16));
+    // }
+
+    // function testPrepend() {
+    //     $list = array(1,2,3);
+    //     $this->assertEquals(array(1,2,3), _prepend($list));
+    //     $this->assertEquals(array(12,1,2,3), _prepend($list, 12));
+    //     $this->assertEquals(array(12,16,1,2,3), _prepend($list, 12, 16));
+    // }
+
+    function testFirst() {
+        $this->assertEquals(null, _first(array()));
+        $this->assertEquals('oh', _first(array('oh','hi')));
+        $this->assertEquals(array('oh', 'hi'), _first(array('oh','hi', 'bro'), 2));
+        $this->assertEquals(array('oh', 'hi', 'bro'), _first(array('oh','hi', 'bro'), 4));
+    }
+
+    function testInitial() {
+        $this->assertEquals(array(), _initial(array()));
+        $this->assertEquals(array('oh','hi'), _initial(array('oh','hi','dude')));
+        $this->assertEquals(array('oh'), _initial(array('oh','hi','dude'), 2));
+        $this->assertEquals(array(), _initial(array('oh','hi','dude'), 3));
+    }
+
+    function testLast() {
+        $this->assertEquals(null, _last(array()));
+        $this->assertEquals('hi', _last(array('oh','hi')));
+        $this->assertEquals(array('hi', 'bro'), _last(array('oh','hi', 'bro'), 2));
+        $this->assertEquals(array('oh', 'hi', 'bro'), _last(array('oh','hi', 'bro'), 4));
+    }
+
+    function testRest() {
+        $this->assertEquals(array(), _rest(array()));
+        $this->assertEquals(array('hi','dude'), _rest(array('oh','hi','dude')));
+        $this->assertEquals(array('dude'), _rest(array('oh','hi','dude'), 2));
+        $this->assertEquals(array(), _rest(array('oh','hi','dude'), 3));
+    }
+
+    function testCompact() {
+        $list = array('',false,0,1);
+        $this->assertEquals(array(1), _compact($list));
+        $this->assertEquals(array(), _compact(array()));
+    }
+
+    function testFlatten() {
+        $list = array(1, array(array(2),3),4);
+        $this->assertEquals(array(1,2,3,4), _flatten($list));
+        $this->assertEquals(array(), _flatten(array()));
+    }
+
+    function testWithout() {
+        $list = array(1,2,1,3);
+        $this->assertEquals(array(2,3), _without($list, 1));
+        $this->assertEquals(array(3), _without($list, 1, 2));
+        $this->assertEquals(array(), _without(array(), 1, 2));
+    }
+
+    function testWith() {
+        $this->assertEquals(array(1), _with(array(), 1));
+        $this->assertEquals(array(1,2,3), _with(array(1,2,3), 1));
+        $this->assertEquals(array(2,3,1), _with(array(2,3), 1));
+    }
+
+    function testUnion() {
+        $this->assertEquals(array(), _union(array()));
+        $this->assertEquals(array(1,2,3,100,12),_union(array(1,2,3),array(1,100,12,2),array(1,2)));
+    }
+
+    function testIntersection() {
+        $this->assertEquals(array(), _intersection(array(),array()));
+        $this->assertEquals(array(1,2),_intersection(array(1,2,3),array(1,100,12,2),array(1,2)));
+    }
+
+    function testUniq() {
+        $this->assertEquals(array(), _uniq(array()));
+        $this->assertEquals(array(1, 2 => 2), _uniq(array(1,1,2,2)));
+    }
+
+    function testZip() {
+        $name = array('drax', 'hulk', 'galactus');
+        $alignment = array('chaotic neutral', 'chaotic good', 'lawful evil');
+        $favoriteDish = array('cheesecake', 'pancake', 'fish stick');
+        $expected = array(
+            array('drax', 'chaotic neutral', 'cheesecake'),
+            array('hulk', 'chaotic good', 'pancake'),
+            array('galactus', 'lawful evil', 'fish stick'),
+        );
+        $this->assertEquals($expected, _zip($name, $alignment, $favoriteDish));
+        $this->assertEquals(array(), _zip(array(),array()));
+    }
+
+    function testIndexOf() {
+        $this->assertEquals(-1, _indexOf(array(), 2));
+        $this->assertEquals(2, _indexOf(array(0,1,2,1), 2));
+        $this->assertEquals(-1, _indexOf(array(0,1,2,1), 3));
+        $this->assertEquals(1, _indexOf(array(0,1,2,1), 1));
+        $this->assertEquals('a', _indexOf(array('a' => 1, 'b' => 1), 1));
+    }
+
+    function testLastIndexOf() {
+        $this->assertEquals(-1, _indexOf(array(), 2));
+        $this->assertEquals(2, _lastIndexOf(array(0,1,2,1), 2));
+        $this->assertEquals(-1, _lastIndexOf(array(0,1,2,1), 3));
+        $this->assertEquals(3, _lastIndexOf(array(0,1,2,1), 1));
+        $this->assertEquals('b', _lastIndexOf(array('a' => 1, 'b' => 1), 1));
+    }
+
+}
+
